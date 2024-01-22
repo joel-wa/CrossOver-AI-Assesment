b8ad67ff4fb8e3850efd9785004701018d7c5a42
joel-wa <UNILIFEGHANA@GMAIL.COM>
2024-01-22 18:06:35 +0530

    -m

--- ai_study/lib/pages/landingPage.dart
+++ ai_study/lib/pages/landingPage.dart
@@ -28,52 +28,54 @@ class _LandingPageState extends State<LandingPage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Welcome to Joel Assiakwa\'s AI Study Prototype',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Welcome to Joel Assiakwa\'s AI Study Prototype',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                'Select the standard of questions you want, to get practice questions:',
-                style: TextStyle(fontSize: 18),
-              ),
-              const SizedBox(height: 20),
-              // List of standards
-              standardOption('4th Grade Common Core Writing Standard'),
-              standardOption(
-                  'Common Core State Standards (CCSS) for English Language Arts'),
-              standardOption(
-                  'National Assessment of Educational Progress (NAEP)'),
-
-              // Text field for user's interest
-              const SizedBox(height: 20),
-              TextField(
-                controller: interestController,
-                onChanged: (value) {
-                  EventHandler().onChangeUserInterest(value);
-                  print(value);
-                },
-                decoration: const InputDecoration(
-                  labelText: 'Type in your interest',
+                const SizedBox(height: 20),
+                const Text(
+                  'Select the standard of questions you want, to get practice questions:',
+                  style: TextStyle(fontSize: 18),
                 ),
-              ),
+                const SizedBox(height: 20),
+                // List of standards
+                standardOption('4th Grade Common Core Writing Standard'),
+                standardOption(
+                    'Common Core State Standards (CCSS) for English Language Arts'),
+                standardOption(
+                    'National Assessment of Educational Progress (NAEP)'),
 
-              // Begin Button
-              const SizedBox(height: 40),
-              ElevatedButton(
-                onPressed: () {
-                  NavClass().changePage('Question Page', context);
-                },
-                child: const Text("Begin"),
-              ),
-            ],
+                // Text field for user's interest
+                const SizedBox(height: 20),
+                TextField(
+                  controller: interestController,
+                  onChanged: (value) {
+                    EventHandler().onChangeUserInterest(value);
+                    print(value);
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Type in your interest',
+                  ),
+                ),
+
+                // Begin Button
+                const SizedBox(height: 40),
+                ElevatedButton(
+                  onPressed: () {
+                    NavClass().changePage('Question Page', context);
+                  },
+                  child: const Text("Begin"),
+                ),
+              ],
+            ),
           ),
         ),
       ),
